a1cd11c976269b0fc6e3f26e70a78f064164641c
omkar04gaikwad <58172164+omkar04gaikwad@users.noreply.github.com>
2020-07-14 14:02:24 +0530

    Update bhp_app.py

--- bhp_app.py
+++ bhp_app.py
@@ -1,7 +1,5 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import re
-import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import chi2_contingency
 from sklearn.model_selection import train_test_split
